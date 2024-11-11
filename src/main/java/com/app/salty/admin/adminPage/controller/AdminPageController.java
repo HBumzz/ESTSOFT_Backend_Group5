f6a39af3d7df0b1699c054ca19a071adb4635b5f
GyuchanMoon <gyuchanm@gmail.com>
2024-11-11 15:41:46 +0900

    어드민페이지 init

--- src/main/java/com/app/salty/admin/adminPage/controller/AdminPageController.java
+++ src/main/java/com/app/salty/admin/adminPage/controller/AdminPageController.java
@@ -1,19 +1,14 @@
 package com.app.salty.admin.adminPage.controller;
 
 import com.app.salty.admin.adminPage.service.AdminService;
-import com.app.salty.admin.challengeBoard.dto.response.ChallengeViewResponse;
-import com.app.salty.admin.challengeBoard.entity.Challenge;
-import com.app.salty.admin.challengeBoard.service.ChallengeBoardService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
-@RestController
+@Controller
 public class AdminPageController {
 
     public final AdminService adminService;
